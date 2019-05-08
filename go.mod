ce1a70552c48c7fc9e72caed9516882bc31fc2f2
micnncim <micnncim@gmail.com>
2019-05-08 10:56:44 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/micnncim/selfupdate-playground
+
+go 1.12
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/rhysd/go-github-selfupdate v1.1.0
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+)
+
+const version = "0.1.0"
+
+func main() {
+	fmt.Println("hello")
+
+	if err := confirmAndSelfUpdate(version); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func confirmAndSelfUpdate(current string) error {
+	latest, found, err := selfupdate.DetectLatest("micnncim/selfupdate-playground")
+	if err != nil {
+		return err
+	}
+
+	v := semver.MustParse(current)
+	if !found || latest.Version.LTE(v) {
+		return nil
+	}
+
+	fmt.Printf("current version: %s, latest version: %s", current, latest.Version)
+	fmt.Print("Do you want to update to", latest.Version, "? (y/n): ")
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil || (input != "y\n" && input != "n\n") {
+		return errors.New("invalid input")
+	}
+	if input == "n\n" {
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	if err := selfupdate.UpdateTo(latest.AssetURL, exe); err != nil {
+		return err
+	}
+
+	log.Println("successfully updated to version", latest.Version)
+	return nil
+}
